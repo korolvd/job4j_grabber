19fdc1f4fcc42252e5f647b9ce5f7151ccdb923b
korolvd <ffirin@mail.ru>
2021-11-11 09:01:47 +0500

    codecov test

--- src/main/java/ru/job4j/Grabber.java
+++ src/main/java/ru/job4j/Grabber.java
@@ -1,6 +1,11 @@
 package ru.job4j;
 
 public class Grabber {
+
+    public static int echo(int value) {
+        return value;
+    }
+
     public static void main(String[] args) {
         System.out.println("grabber");
     }
