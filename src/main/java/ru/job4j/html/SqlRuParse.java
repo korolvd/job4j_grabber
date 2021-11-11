1fdbe2db4a219ed7d1ec0d061b35b482a148c3ed
korolvd <ffirin@mail.ru>
2021-11-11 19:08:13 +0500

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,15 +5,21 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.List;
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element post : row) {
-            Element parent = post.parent();
-            System.out.print(parent.children().get(5).text() + " ");
-            System.out.println(post.child(0).text());
-            System.out.println(post.child(0).attr("href"));
+        String url = "https://www.sql.ru/forum/job-offers";
+        List<String> pages = List.of("", "/2", "/3", "/4", "/5");
+        for (String page : pages) {
+            Document doc = Jsoup.connect(url + page).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element post : row) {
+                Element parent = post.parent();
+                System.out.print(parent.children().get(5).text() + " ");
+                System.out.println(post.child(0).text());
+                System.out.println(post.child(0).attr("href"));
+            }
         }
     }
 }
