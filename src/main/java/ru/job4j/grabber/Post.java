a862acba1110a1ee7071350a25c212afcc0a157e
korolvd <ffirin@mail.ru>
2021-11-12 16:44:25 +0500

    2.2. Модель данных - Post. [#285211 #227418]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -15,6 +15,17 @@ public class Post {
 
     private LocalDateTime created;
 
+    public Post() {
+
+    }
+
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
     public int getId() {
         return id;
     }
