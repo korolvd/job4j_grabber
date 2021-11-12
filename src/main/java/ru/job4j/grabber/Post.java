904883503cfc4c86b2a3452e5d80f22f46a65671
korolvd <ffirin@mail.ru>
2021-11-12 14:28:00 +0500

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -15,6 +15,30 @@ public class Post {
 
     private LocalDateTime created;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
     public String getDescription() {
         return description;
     }
---
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -0,0 +1,52 @@
+package ru.job4j.grabber;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SqlRuParse implements Parse {
+
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> list = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements rows = doc.select(".postslisttopic");
+            rows.forEach(r -> list.add(details(r.child(0).attr("href"))));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    @Override
+    public Post details(String link) {
+        Post post = new Post();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Element footer = doc.select(".msgFooter").get(0);
+            Element title = footer.parent().parent().child(0).child(0);
+            Element description = footer.parent().parent().child(1).child(1);
+            LocalDateTime created = dateTimeParser.parse(footer.text().split(" \\[")[0]);
+            post.setTitle(title.text());
+            post.setLink(link);
+            post.setDescription(description.text());
+            post.setCreated(created);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+}
