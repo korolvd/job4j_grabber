4f38d7d0badd3ba439ce4fa3a4acd6f0c7995a64
korolvd <ffirin@mail.ru>
2021-11-12 12:23:59 +0500

    2.2. Модель данных - Post. [#285211 #227418]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -24,15 +24,15 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return Objects.equals(title, post.title)
+        return id == post.id
+                && Objects.equals(title, post.title)
                 && Objects.equals(link, post.link)
-                && Objects.equals(description, post.description)
                 && Objects.equals(created, post.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, link, description, created);
+        return Objects.hash(id, title, link, created);
     }
 
     @Override
